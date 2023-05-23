f106d2ca10d7d261e0ba9d56c1b71ecfc316703c
mahmudumar24859 <mahmudumar24859@gmail.com>
2023-05-23 17:31:29 +0100

    Fixed Routes

--- team1/index/views.py
+++ team1/index/views.py
@@ -5,3 +5,15 @@ from django.shortcuts import render
 
 def home_view(request):
     return render(request, "home.html")
+
+
+def recycler_portal(request):
+    return render(request, "recycler.html")
+
+
+def support_recycler(request):
+    return render(request, "support.html")
+
+
+def recycle_data(request):
+    return render(request, "recycle_data.html")
